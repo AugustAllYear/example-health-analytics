267605a9f5695c79a81574e1edb7869ddf829560
August <augustvollbrecht@gmail.com>
2026-05-02 10:37:18 -0400

    Delete 02_load.sql

--- 02_load.sql
+++
@@ -1,7 +0,0 @@
--- Load Parquet (claims)
-CREATE OR REPLACE TABLE	staging.claims_raw AS
-SELECT * FROM read_parquet('data/claims.parquet');
-
--- Load JSON (providers) - semi-structured
-CREATE OR REPLACE TABLE staging.providers_raw AS
-SELECT * FROM read_json_auto('auto/providers.json');
